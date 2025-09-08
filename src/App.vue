d7bad20fbff2dd6d2d7fd80aea70d75bd2de4813
Aris Karamat <karamataris@gmail.com>
2025-09-08 16:40:26 +0200

    added BaseCard Component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import BaseCard from './components/BaseCard.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<BaseCard id="base-card">
+    <h1>Überschrift</h1>
+    <p>Dies ist ein Beispieltext, der in die Card-Komponente eingefügt wird.</p>
+  </BaseCard>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+#base-card{
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
 }
 </style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.card {
+    background-color: #011F35;
+    border-radius: 16px;
+    padding: 24px 32px;
+    color: white;
+}
+</style>
